98968aab4d3c97c12a22894b052749bb63ca7e62
toyondiamond <eplmpopova@mail.ru>
2022-12-01 21:09:37 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,45 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        // task1 and 2
-        int [] zp = new int[30];
-        int sum = 0;
-        int min = 10_000;
-        int max = -1;
+        int sum1 = 0;
+        int[] arr = generateRandomArray();
+        for (int value : arr) {
+            sum1 += value;
+        }
+        System.out.println("Сумма трат за месяц составила " + sum1);
 
-        for (int i = 0; i < zp.length; i++) {
-            zp[i] = (int) (Math.random() * 500) + 200;
-            sum += zp[i];
-            if (zp[i] < min) {
-                min = zp[i];
-            }
-            if (zp[i] > max) {
-                max = zp[i];
+        int [] array = generateRandomArray();
+        int min = array[0];
+        int max = array[0];
+        for (int element: array){
+            if (element<min){
+            } else if (element>max) {
+                max = element;
             }
         }
-        double sredniyRaschod = sum / 30;
-            System.out.println(Arrays.toString(zp));
-            System.out.println("Итого " + sum);
-            System.out.println("Минимальная зарплата " + min);
-            System.out.println("Максимальная зарплата " + max);
-             System.out.println("Средняя сумма трат за месяц составила  " + sredniyRaschod + " руб");
+        System.out.println("Минимальные затраты составили "+ min);
+        System.out.println("Максимальные затраты составили "+ max);
+
+        int [] arr3  = generateRandomArray();
+        int sum3 = 0;
+        for(int i:arr3){
+            sum3 += i;
+        }
+        System.out.println("Средняя сумма трат за месяц составила " + sum3/(float)arr3.length);
+        char [] reverseFullName  = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length -1; i >=0; i--) {
+            System.out.print(reverseFullName[i]);
+        }
+    }
+
+    public static int[] generateRandomArray() {
+
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+            sum += arr[i];
+        }
+        return arr;
     }
 }
